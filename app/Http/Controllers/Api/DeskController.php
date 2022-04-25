ad12a461c222e3a0bf6fdff2da0b44df6e2f9f1d
David Muradyan <76254008+dav1710@users.noreply.github.com>
2022-04-25 23:13:28 +0400

    first

---
+++ app/Http/Controllers/Api/DeskController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\DeskStoreRequest;
+use App\Http\Resources\DeskResource;
+use App\Models\Desk;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\Console\Input\Input;
+
+class DeskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return DeskResource::collection(Desk::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(DeskStoreRequest $request)
+    {
+        $created_desk = Desk::create($request->validated());
+
+        return new DeskResource($created_desk);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {   
+        $desk = Desk::where('token' , $id)->first();
+        $desk->delete();
+        return response()->json([
+            'success' => false,
+            'message' => 'Տվյալը գտնված է և ջնջված բազայից'
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(DeskStoreRequest $request, Desk $desk)
+    {
+        $desk->update($request->validated());
+
+        return new DeskResource($desk);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $desk = Desk::where('token' , $id)->first();
+        $desk->delete();
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Տվյալը գտնված է և ջնջված բազայից'
+        ]);
+    }
+}
---
+++ database/factories/DeskListFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Desk;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DeskListFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'desk_id' => Desk::get()->random()->id,
+        ];
+    }
+}
